7598bbdd32295cfd3f0fa31020a396b63c52f16b
JhonellaJazmine <orquillojhonella@gmail.com>
2023-03-29 22:26:09 +0800

    initial commit

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = 'dgbq5v6dk'
+    config.api_key = '191631119444689'
+    config.api_secret = 'yqINNuZ49sHBIevlolhA-zia_l0'
+    config.secure = true
+  end
+  
